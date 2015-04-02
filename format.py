7afbed1bede6fb67780547081b148dc47affafad
Uberi <azhang9@gmail.com>
2015-04-01 21:12:13 -0400

    Add .gitignore and message JSON formatter.

---
+++ format.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import json, sys
+
+messages = json.load(sys.stdin)
+
+# sort by timestamp, format entries on line, and sort keys in each entry
+messages = sorted(messages, key=lambda message: -message["timestamp"])
+result = "[\n" + ",\n".join(json.dumps(entry, sort_keys=True) for entry in messages) + "\n]\n"
+
+sys.stdout.write(result)
