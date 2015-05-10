6a3281a7f78d874b0313e06f0ede36161b1f8f9d
Uberi <azhang9@gmail.com>
2015-05-10 15:24:27 -0400

    Stuff.

--- word-cloud/example_create_word_cloud.sh
+++ word-cloud/example_create_word_cloud.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 word_cloud_generator.py mask-and.png word-cloud-and.png "Anthony Zhang" < ../data/normalized_data.json
+python3 word_cloud_generator.py "Anthony.*" word-cloud-and.png mask-and.png < ../data/normalized_data.json
--- word-cloud/word_cloud_generator.py
+++ word-cloud/word_cloud_generator.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 
-import os, json, sys
+import os, json, sys, re
 from scipy import misc
 
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator, random_color_func
 
-if not (3 <= len(sys.argv) <= 4):
-    print("Usage: {} MASK OUTPUT_FILE [USER_FILTER] < NORMALIZED_DATA".format(sys.argv[0]))
+# process arguments
+if not (4 <= len(sys.argv) <= 5):
+    print("Usage: {} USER_REGEX OUTPUT_PNG_FILE MASK_IMAGE [COLOR_IMAGE] < NORMALIZED_DATA_JSON".format(sys.argv[0]))
     sys.exit(1)
+user_regex = sys.argv[1]
+output_png = sys.argv[2]
+mask_image = sys.argv[3]
+color_image = None if len(sys.argv) < 5 else sys.argv[4]
 
-mask_file = sys.argv[1]
-output_file = sys.argv[2]
-user = sys.argv[3] if len(sys.argv) >= 4 else None
 ignored_words = STOPWORDS
 
 # load message text
 messages = json.load(sys.stdin)
-text = "\n".join(m[2] for m in messages) if user is None else "\n".join(m[2] for m in messages if m[1] == user)
+pattern = re.compile(user_regex)
+text = "\n".join(m[2] for m in messages if pattern.fullmatch(m[1]))
 
 # generate word cloud
-mask = misc.imread(mask_file)
-wc = WordCloud(background_color="white", max_words=10000, mask=mask, stopwords=ignored_words, scale=4)
+mask = misc.imread(mask_image)
+coloring_function = ImageColorGenerator(misc.imread(color_image)) if color_image is not None else random_color_func
+wc = WordCloud(background_color="white", max_words=10000, mask=mask, stopwords=ignored_words, scale=4, color_func=coloring_function)
 wc.generate(text)
 
-# store the word cloud image in a file
-wc.to_file(output_file)
+# save the word cloud image to stdout
+wc.to_file(output_png)
